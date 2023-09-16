1a4aab3448ee373dafb9acde13a8d8124a4617c0
xx <xx>
2023-09-16 21:06:08 +0600

    testing

---
+++ testing.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void tcase() {
+	cout << "HI";
+}
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t = 1;
+	//cin >> t;
+
+	while (t-- > 0)
+		tcase();
+}
