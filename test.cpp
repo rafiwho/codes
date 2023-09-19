bdb3de0720935188ce5d563b68217f6f10001041
x <x>
2023-09-19 22:25:12 +0600

    x

--- test.cpp
+++ test.cpp
@@ -2,14 +2,30 @@
 using namespace std;
 
 void tcase() {
-	
+	int n;
+	cin >> n;
+	vector<int>v;
+	int64_t sum = 0;
+	for (int i = 0; i < n; ++i) {
+		int duration, deadline;
+		cin >> duration >> deadline;
+		v.push_back(duration);
+		sum += deadline;
+	}
+	sort(v.begin(), v.end());
+	int64_t ans = 0;
+	for (auto duration : v) {
+		ans += duration;
+		sum -= ans;
+	}
+	cout << sum << '\n';
 }
 int32_t main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	int t = 1;
-	cin >> t;
+	//cin >> t;
 
 	while (t-- > 0)
 		tcase();
