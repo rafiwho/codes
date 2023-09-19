2d8fc6939dc1fa23156c09e94690306d478ebc83
x <x>
2023-09-19 22:26:21 +0600

    x

--- test.cpp
+++ test.cpp
@@ -4,21 +4,19 @@ using namespace std;
 void tcase() {
 	int n;
 	cin >> n;
-	vector<int>v;
-	int64_t sum = 0;
+	multiset<int>ms;
 	for (int i = 0; i < n; ++i) {
-		int duration, deadline;
-		cin >> duration >> deadline;
-		v.push_back(duration);
-		sum += deadline;
+		int x; cin >> x;
+		auto it = ms.upper_bound(x);
+		if (it == ms.end()) {
+			ms.insert(x);
+		} else {
+			ms.erase(it);
+			ms.insert(x);
+		}
 	}
-	sort(v.begin(), v.end());
-	int64_t ans = 0;
-	for (auto duration : v) {
-		ans += duration;
-		sum -= ans;
-	}
-	cout << sum << '\n';
+
+	cout << int(ms.size()) << '\n';
 }
 int32_t main() {
 	ios_base::sync_with_stdio(false);
