77ece9f4f4727e88889089b1e313164674f082c8
x <x>
2023-09-21 16:23:01 +0600

    x

--- test.cpp
+++ test.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 
 void tcase() {
-	int n;
-	cin >> n;
-	multiset<int>ms;
-	for (int i = 0; i < n; ++i) {
-		int x; cin >> x;
-		auto it = ms.upper_bound(x);
-		if (it == ms.end()) {
-			ms.insert(x);
-		} else {
-			ms.erase(it);
-			ms.insert(x);
-		}
-	}
-
-	cout << int(ms.size()) << '\n';
 }
 int32_t main() {
 	ios_base::sync_with_stdio(false);
