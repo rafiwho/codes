909fafde93d27dc8261114bdc573eeeadf3d3e2d
x <x>
2023-09-23 14:43:56 +0600

    x

--- Almost_All_Divisors.cpp
+++ Almost_All_Divisors.cpp
@@ -1,14 +1,44 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+constexpr int N = 1e6 + 7;
 
 void tcase() {
+	int n;
+	cin >> n;
+	vector<int>v(n);
+	bitset<N>bs;
+	bs.reset();
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i];
+		bs[v[i]] = 1;
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; ++i) {
+		for (int j = 2; 1LL * j * j <= v[i]; ++j) {
+			if (v[i] % j == 0) {
+				if (!bs[v[i] / j] || !bs[j]) {
+					cout << -1 << '\n';
+					return;
+				}
+			}
+		}
+	}
+	int64_t ans = 1LL * v[0] * v[n - 1];
+	for (int i = 0; i < n; ++i) {
+		if (1LL * v[i]*v[n - i - 1] != ans) {
+			cout << -1 << '\n';
+			return;
+		}
+	}
+	cout << ans << '\n';
 }
 int32_t main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+	cin.tie(NULL);
 
 	int t = 1;
-	//cin >> t;
+	cin >> t;
 
 	while (t-- > 0)
 		tcase();
