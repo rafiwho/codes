dfe0b152e1afc94323fad6bce4f8062d1dfe7c21
x <x>
2023-10-16 15:10:10 +0600

    x

--- make_input_output.cpp
+++ make_input_output.cpp
@@ -5,16 +5,34 @@ mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
 void tcase() {
 
-	const int mx_input_output = 100;
+	const int mx_input_output = 10;
 	for (auto i = 1LL; i <= mx_input_output ; ++i) {
 		// fun is the sol of a given prob
 		auto fun = [&](int x, int y) {
 			ofstream myfle("input" + to_string(i) + ".txt"); // input1....100.txt
 			myfle << x << " " << y; // pushing to my input file
+			int ticket_price = 0;
+
+			if (x < 3 || x >= 70) {
+				ticket_price = 0;
+			} else if (x >= 3 && x <= 12) {
+				ticket_price = 10;
+			} else if (x >= 65 && x <= 69) {
+				ticket_price = 15;
+			} else if (x >= 30 && x <= 40 && y > 75) {
+				ticket_price = 18;
+			} else {
+				ticket_price = 20;
+			}
 			ofstream myfile("output" + to_string(i) + ".txt");// output1...100.txt
-			myfile << 69; // pushing to my output file
+
+			if (ticket_price == 0) {
+				myfile << "Free\n"; // pushing to my output fill
+			} else {
+				myfile << ticket_price << '\n'; // pushing to my output file
+			}
 		};
-		fun(rng() % 100, rng() % 200);
+		fun(rng() % 100 + 1, rng() % 200 + 1);
 		// ranges
 	}
 }
