1afe71ceecf7686ec2e3027bb6d832f3935b36a4
a <a>
2023-11-19 15:43:54 +0600

    x

--- sum of xor of all subseq.cpp
+++ sum of xor of all subseq.cpp
@@ -1,8 +1,12 @@
+// or of whole array * 2^n-1
 #include <bits/stdc++.h>
 using namespace std;
 
 void tcase() {
-
+	vector<int> arr = {1, 2, 3, 4};
+	int orr = (1 | 2 | 3 | 4);
+	int ans = orr * (1 << arr.size());
+	cout << ans << '\n';
 }
 int32_t main() {
 	ios_base::sync_with_stdio(false);
