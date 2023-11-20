3cb7a6a7e6dc3a10f7cee90c1fb79c7c92c6e283
a <a>
2023-11-20 15:59:32 +0600

    x

--- test.cpp
+++ test.cpp
@@ -1,16 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
 
-void tcase() {
+struct node {
+	int data;
+	struct node*left;
+	struct node*right;
+};
 
+struct node* create(int data) {
+	struct node *newnode =  (struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return newnode;
 }
-int32_t main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
 
-	int t = 1;
-	//cin >> t;
+struct node* insert(struct node *root, int data) {
+	if (root == NULL) {
+		return create(data);
+	}
+	if (data < root->data) {
+		root->left = insert(root->left, data);
+	} else if (data > root->data) {
+		root->right = insert(root->right, data);
+	}
+	return root;
+}
 
-	while (t-- > 0)
-		tcase();
+void traverse(struct node * root) {
+	if (root == NULL) {
+		return ;
+	}
+	traverse(root->left);
+	printf("%d ", root->data);
+	traverse(root->right);
+}
+
+int main() {
+	struct node* root = NULL;
+	root = insert(root, 1);
+	insert(root, 2);
+	insert(root, 3);
+	insert(root, 4);
+	insert(root, 5);
+	traverse(root);
 }
