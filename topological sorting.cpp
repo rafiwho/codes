2d8a08192d727aaf4c8bed4417cc2a1d59cc03d4
Md. Uodoy Hossan Rafi <144991940+rafiwho@users.noreply.github.com>
2024-05-18 02:01:25 +0600

    Update topological sorting.cpp
    fixed

--- topological sorting.cpp
+++ topological sorting.cpp
@@ -3,43 +3,43 @@
 using namespace std;
 constexpr int N = 1e5;
 void tcase() {
-	int n, m;
-	cin >> n >> m;
-	vector<int> graph[N];
-	int indegree[N] {};
-	queue<int> q;
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		graph[a].push_back(b);
-		indegree[b]++;
-	}
-	for (int i = 1; i <= n; ++i) {
-		if (indegree[i] == 0)
-			q.emplace(i);
-	}
-	vector<int>order;
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop(); order.emplace_back(cur);
-			if (--indegree[child] == 0) {
-		for (int child : graph[cur]) {
-				q.emplace(child);
-			}
-		}
-	}
+    int n, m;
+    cin >> n >> m;
+    vector<int> graph[N];
+    int indegree[N] {};
+    queue<int> q;
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        indegree[b]++;
+    }
+    for (int i = 1; i <= n; ++i) {
+        if (indegree[i] == 0)
+            q.emplace(i);
+    }
+    vector<int>order;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop(); order.emplace_back(cur);
+        for (int child : graph[cur]) {
+            if (--indegree[child] == 0) {
+                q.emplace(child);
+            }
+        }
+    }
 
-	for (auto x : order)
-		cout << x << ' ';
-	cout << '\n';
+    for (auto x : order)
+        cout << x << ' ';
+    cout << '\n';
 }
 int32_t main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int t = 1;
-	//cin >> t;
+    int t = 1;
+    //cin >> t;
 
-	while (t-- > 0)
-		tcase();
+    while (t-- > 0)
+        tcase();
 }
