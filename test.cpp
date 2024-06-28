72024a27a6ace153cc1dbc6aec6517ecf455dd86
x <X>
2024-06-28 14:31:32 +0600

    x

--- test.cpp
+++ test.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-auto tcase()->void{
+void tcase() {
 	int n;
 	cin >> n;
-	cout << n;
+	vector<int>v(n);
+	for (int &x : v)
+		cin >> x;
+
+	std::map<int , int> fre;
+	int64_t ans = 0;
+
+	for (int i = 0; i < n; ++i) {
+		for (int k = 0; k <= 30; ++k) {
+			if (fre.contains((1 << k) - v[i])) {
+				ans += fre[(1 << k) - v[i]];
+			}
+		}
+		fre[v[i]]++;
+	}
+	cout << ans << '\n';
 }
-auto main()->int32_t{
+int32_t main() {
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
