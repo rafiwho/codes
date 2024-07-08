8813d0726bfb17598d7aa1f65db3be799d3e89bd
x <X>
2024-07-09 04:07:57 +0600

    x

--- all pair sumer sum.cpp
+++ all pair sumer sum.cpp
@@ -3,12 +3,7 @@ using namespace std;
 
 void tcase() {
 	vector<int>v = {1, 2, 3};
-	int n = 3; // every v[i] will contribute 2*n*v[i]
-	int ans = 0 ;  // (_n_,_n_) 2*n contribution of every ele
-	for (int i = 0; i < n; ++i) {
-		ans += (n << 1) * v[i];
-	}
-	cout << ans << '\n';
+	// ans += v[i] * i * n - i - 1
 }
 int32_t main() {
 	ios_base::sync_with_stdio(false);
--- test.cpp
+++ test.cpp
@@ -4,11 +4,19 @@ using namespace std;
 void tcase() {
 	int n;
 	cin >> n;
-	int cur = 0;
+	vector<int>v[n + 1];
 	for (int i = 1; i < n; ++i) {
-		int x; cin >> x >> x;
-		cout << cur++ << '\n';
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
 	}
+	long long ans = 0 ;
+	for (int i = 1; i <= n; ++i) {
+		int sz = v[i].size();
+		ans += sz * (sz - 1) / 2;
+	}
+	cout << ans << '\n';
 }
 int32_t main() {
 
