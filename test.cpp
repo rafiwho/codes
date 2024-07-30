6cb39bfa6993cf425aeeae6ab2554df094ec9556
x <X>
2024-07-30 11:29:02 +0600

    x

--- test.cpp
+++ test.cpp
@@ -1,26 +1,16 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// void tcase() {
-// 	int n, x;
-// 	cin >> n >> x;
-// 	string s;
-// 	cin >> s;
+void tcase() {
+}
+int32_t main() {
 
-// }
-// int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(nullptr);
-// #ifdef ONLINE_JUDGE
-// 	freopen("binary.in", "r", stdin);
-// #endif
-// 	int32_t t = 1;
-// 	//cin >> t;
+	int32_t t = 1;
+	//cin >> t;
 
-// 	while (t-- > 0)
-// 		tcase();
-// }
-sudo add - apt - repository ppa: danielrichter2007 / grub - customizer
-sudo apt update
-sudo apt install grub - customizer
+	while (t-- > 0)
+		tcase();
+}
