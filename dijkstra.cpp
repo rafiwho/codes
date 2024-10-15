0c74afc6b35ee4d61811510c1ae03fe659f0e792
f <f>
2024-10-15 10:19:41 +0600

    x

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -4,43 +4,53 @@ using i64 = long long;
 template <typename T>
 using min_heap = priority_queue<T, vector<T>, greater<T>>;
 constexpr i64 INF = 1E15;
-constexpr int N = 2E5+5;
-std::vector<array<int,2>> v[N];
+constexpr int N = 2E5 + 5;
+std::vector<array<int, 2>> v[N];
 void tcase() {
+
 	int n; cin >> n;
-	for (int i = 1; i <= n-1; ++i) {
-		int a,b,x; cin >> a >> b >> x;
-		v[i].push_back({i+1,a});
-		v[i].push_back({x,b});
+	for (int i = 1; i <= n - 1; ++i) {
+		int a, b, x; cin >> a >> b >> x;
+		v[i].push_back({i + 1, a});
+		v[i].push_back({x, b});
 	}
+
 	auto dijkstra = [&](int source)->i64{
-		min_heap<array<i64,2>>q;
-		vector<bool>vis(N,false);
-		vector<i64>dis(N,INF);
-		q.push({0,source});
+
+		min_heap<array<i64, 2>>q;
+		vector<bool>vis(N, false);
+		vector<i64>dis(N, INF);
+
+		q.push({0, source});
 		dis[source] = 0;
-		while(!q.empty()){
-			auto [cur_w,cur_v] = q.top();
-			q.pop(); if(vis[cur_v])continue;
+
+		while (!q.empty()) {
+			auto [cur_w, cur_v] = q.top();
+			q.pop(); if (vis[cur_v])continue;
 			vis[cur_v] = true;
-			for(auto [c_v,c_w]:v[cur_v]){
-				if(dis[cur_v] + c_w < dis[c_v]){
+
+			for (auto [c_v, c_w] : v[cur_v]) {
+
+				if (dis[cur_v] + c_w < dis[c_v]) {
 					dis[c_v] = dis[cur_v] + c_w;
-					q.push({dis[c_v],c_v}); 
+					q.push({dis[c_v], c_v});
 				}
+
 			}
 		}
+
 		return dis[n];
 	};
+
 	cout << dijkstra(1) << '\n';
 }
 int32_t main() {
-   ios_base::sync_with_stdio(false);
-   cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-   int t = 1;
-   //cin >> t;
+	int t = 1;
+	//cin >> t;
 
-   while (t-- > 0)
-     tcase();
+	while (t-- > 0)
+		tcase();
 }
--- test.cpp
+++ test.cpp
@@ -1,32 +1,111 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int>get_div(int n) {
-    vector<int>divs;
-    for (int i = 1; i * i <= n; ++i) {
-        if (n % i == 0) {
-            divs.push_back(i);
-            divs.push_back(n / i);
+
+vector<int> z_function(const string &s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; ++i) {
+        if (i < r) {
+            z[i] = min(r - i, z[i - l]);
+        }
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] > r) {
+            l = i;
+            r = i + z[i];
         }
     }
-    sort(divs.begin(), divs.end());
-    divs.erase(unique(divs.begin(), divs.end()), divs.end());
-    return divs;
+    return z;
 }
-#include "test.h"
+int mrge(string &a, string &b) {
+    string con = b + "$" + a;
+    vector<int> z = z_function(con);
+    int mx = 0;
+    for (int i = b.size() + 1; i < z.size(); ++i) {
+        if (z[i] == con.size() - i) {
+            mx = z[i];
+            break;
+        }
+    }
+    return mx;
+}
+
+template <typename T>
+void ckmax(T &a, T b) {
+    a = a > b ? a : b;
+}
+
+int n;
+const int N = 11;
+vector<vector<int>> gain(N, vector<int>(N, 0));
+vector<vector<int>> dp(N, vector<int>(1 << N, -1));
+
+int f(int i, int mask) {
+    if (__builtin_popcount(mask) == n) return 0;
+    int &ans = dp[i][mask];
+    if (ans != -1) return ans;
+    ans = 0;
+    for (int k = 0; k < n; ++k) {
+        if (!(mask & (1 << k))) {
+            ans = max(ans, gain[i][k] + f(k, mask | (1 << k)));
+        }
+    }
+    return ans;
+}
 void tcase() {
-    int n;
     cin >> n;
-    cout << (get_div(n).size() & 1 ? "FastestFinger\n" : "Ashishgup\n");
+    vector<string> v(n);
+    int ans = 0;
+    int emnie = 0;
+    for (auto &s : v) {
+        cin >> s;
+        ans += s.size();
+    }
+    sort(v.begin(), v.end(), [](auto & a, auto & b) { return a.size() > b.size(); });
 
+    vector<bool> bad(n, false);
+    for (int i = 0; i < n; ++i) {
+        if (!bad[i]) {
+            for (int j = i + 1; j < n; ++j) {
+                if (!bad[j] && mrge(v[i], v[j]) == v[j].size()) {
+                    bad[j] = true;
+                    emnie += v[j].size();
+                }
+            }
+        }
+    }
+
+    vector<string> nv;
+    for (int i = 0; i < n; ++i) {
+        if (!bad[i]) {
+            nv.push_back(v[i]);
+        }
+    }
+    swap(nv, v);
+    n = v.size();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i != j) {
+                gain[i][j] = mrge(v[i], v[j]);
+            }
+        }
+    }
+    int best = 0;
+    for (int i = 0; i < n; ++i) {
+        ckmax(best, f(i, 1 << i));
+    }
+    cout << ans - (best + emnie) << '\n';
 }
-int32_t main() {
 
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int32_t t = 1;
-    cin >> t;
-
-    while (t-- > 0)
+    int t = 1;
+    while (t--) {
         tcase();
+    }
 }
