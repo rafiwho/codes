0d650eac91e6f382c65116131013a23b8e9c9067
f <f>
2024-10-30 21:56:17 +0600

    Updated by Simple Git GUI

--- test.cc
+++ test.cc
@@ -6,12 +6,12 @@ void tcase() {
 }
 int32_t main() {
 
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-  int32_t t = 1;
-  //cin >> t;
+	int32_t t = 1;
+	//cin >> t;
 
-  while (t-- > 0)
-    tcase();
+	while (t-- > 0)
+		tcase();
 }
