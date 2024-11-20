197a855b5d077a9fbdfeffd97161f37b3a12e315
rafiwho <uodoyhossanrafi420@gmail.com>
2024-11-20 20:13:26 +0600

    hi

--- check.cc
+++ check.cc
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void tcase() {
-}
+
+void tcase() {}
 int32_t main() {
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	int32_t t = 1;
-	//cin >> t;
+	// cin >> t;
 
 	while (t-- > 0)
 		tcase();
