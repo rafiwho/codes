90628e70da964430495879feb409359f5a124c85
rafiwho <uodoyhossanrafi420.com>
2024-12-01 01:56:54 +0600

    x

--- make_input_output.cc
+++ make_input_output.cc
@@ -7,33 +7,22 @@ void tcase() {
 
 	const int mx_input_output = 10;
 	for (auto i = 1LL; i <= mx_input_output ; ++i) {
+
 		// fun is the sol of a given prob
 		auto fun = [&](int x, int y) {
-			ofstream myfle(to_string(i) + ".in"); // input1....100.txt
-			myfle << x << " " << y; // pushing to my input file
-			int ticket_price = 0;
 
-			if (x < 3 || x >= 70) {
-				ticket_price = 0;
-			} else if (x >= 3 && x <= 12) {
-				ticket_price = 10;
-			} else if (x >= 65 && x <= 69) {
-				ticket_price = 15;
-			} else if (x >= 30 && x <= 40 && y > 75) {
-				ticket_price = 18;
-			} else {
-				ticket_price = 20;
-			}
+			ofstream myfle(to_string(i) + ".in"); // input1....100.txt
+
+			// myfle << x << " " << y; // pushing to my input file
+
 			ofstream myfile(to_string(i) + ".out");// output1...100.txt
 
-			if (ticket_price == 0) {
-				myfile << "Free\n"; // pushing to my output fill
-			} else {
-				myfile << ticket_price << '\n'; // pushing to my output file
-			}
+			// myfile << "something\n"; // pushing to my output fill
+
 		};
 		fun(rng() % 100 + 1, rng() % 200 + 1);
 		// ranges
+
 	}
 }
 int32_t main() {
--- test.cc
+++ test.cc
@@ -1,24 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<int, int64_t> m;
-void ins(int a, int64_t b) {
-	auto it = m.lower_bound(a);
-	if (it != end(m) && it->second >= b) return;
-	it = m.insert(it, {a, b});
-	it->second = b;
-	while (it != begin(m) && prev(it)->second <= b) m.erase(prev(it));
-}
-int64_t query(int x) {
-	auto it = m.lower_bound(x);
-	return it == end(m) ? 0 : it->second;
-}
 void tcase() {
-	int n; cin >> n;
 
-	for (int i = 0; i < n; ++i) {
-
-	}
 }
 int32_t main() {
 
@@ -31,6 +15,3 @@ int32_t main() {
 	while (t-- > 0)
 		tcase();
 }
-
-{a, b} {c, d}
-abs(a - c) + abs(c - d);
